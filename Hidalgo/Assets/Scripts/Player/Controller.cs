4397c4411af4dfb539d4c8b7bdc651a0d69c9380
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-04-21 13:13:12 -0300

    Movimiento en 2 ejes topdown
    Dividí el movimiento en un controller que recibe la acción y se la envía a un script de movement para que la realice.

---
+++ Hidalgo/Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Controller
+{
+    Movement _movement;
+
+    public Controller(Movement m)
+    {
+        _movement = m;
+    }
+
+    public void OnUpdate()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = +1f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY = +1f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
+
+        
+        
+         _movement.Move(moveX, moveY);
+        
+
+    }
+}
---
+++ Hidalgo/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D rigidbody2D;
+    public float speed;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    Controller _controller;
+    Movement _movement;
+
+
+    private void Start()
+    {
+        _movement = new Movement(this);
+        _controller = new Controller(_movement);
+    }
+
+    private void Update()
+    {
+        _controller.OnUpdate();
+    }
+}
