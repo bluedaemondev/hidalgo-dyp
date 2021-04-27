203ab56527dca3d8e2511d19cb1b3b4b80fdd6c2
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-04-27 03:46:43 -0300

    pickup scene controller, pickup manager+ ui display

---
+++ Hidalgo/Assets/PickupCounterUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Image = UnityEngine.UI.Image;
+
+public class PickupCounterUI : MonoBehaviour
+{
+    public List<Sprite> spriteStates;
+    [SerializeField] private int currentIndex;
+    [SerializeField] private Image image;
+
+    private void Awake()
+    {
+        if (!image)
+            image = GetComponent<Image>();
+    }
+
+    public void StepNextSprite()
+    {
+        if(currentIndex < spriteStates.Count - 1)
+            currentIndex++;
+
+        image.sprite = spriteStates[currentIndex];
+        CheckEndList();
+    }
+
+    void CheckEndList()
+    {
+        if (currentIndex >= spriteStates.Count) { 
+            GameSceneManagerPickupsLevel.instance.SetPickupsCompletedState();
+            this.enabled = false;
+        }
+    }
+}
---
+++ Hidalgo/Assets/_scripts/GameSceneManagerPickupsLevel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Manager de acciones en la escena para el nivel top down en el que llevas a 
+/// rocinante y sancho hasta la meta
+/// </summary>
+public class GameSceneManagerPickupsLevel : MonoBehaviour
+{
+    public static GameSceneManagerPickupsLevel instance { get; private set; }
+    public event Action onGameWin;
+    public event Action onGameLose;
+
+    private bool pickupsCompleted = false;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance != null)
+            Destroy(instance);
+
+        instance = this;
+
+    }
+
+    public void Win()
+    {
+        
+        if (onGameWin != null && pickupsCompleted)
+        {
+            onGameWin();
+        }
+    }
+
+    public void SetPickupsCompletedState()
+    {
+        pickupsCompleted = true;
+    }
+}
