f649f6efcd9ad73af25d5b98de5be4971d3c4f82
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-04-27 16:25:41 -0300

    Footsteps

--- Hidalgo/Assets/Scripts/Player/Movement.cs
+++ Hidalgo/Assets/Scripts/Player/Movement.cs
@@ -5,7 +5,7 @@ public class Movement
 {
     Player _player;
     private Vector3 moveDir;
-
+    float nextSoundTime = 0;
     public Movement(Player p)
     {
         _player = p;
@@ -19,9 +19,19 @@ public class Movement
         _player.GetComponent<Rigidbody2D>().velocity = moveDir * _player.Speed;
 
         if (_player.GetComponent<Rigidbody2D>().velocity.magnitude > 0.1f)
-            _player.GetComponent<AnimatedCharacterController>().State = CharacterState.MOVING;            
+        {
+            _player.GetComponent<AnimatedCharacterController>().State = CharacterState.MOVING;
+
+            if(Time.time>=nextSoundTime)
+            {
+                SoundManager.instance.PlayEffect(_player.footsteps);
+                nextSoundTime = Time.time + _player.footsteps.length;
+            }
+        }
         else
+        {
             _player.GetComponent<AnimatedCharacterController>().State = CharacterState.IDLE;
+        }
 
     }
 }
