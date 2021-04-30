39b0e2956f4211f67cb9f5a1eeb15648b5f60769
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-04-30 03:17:15 -0300

    hub prototype + level selection
    prototipo para probar. faltaria mostrar UI popup en vez de cambiar directamente la escena. hay que agregar un delegate en gamemanager o scenemanager para que recopile todas las acciones al interactuar con un trigger, recibiendo un id de nivel
    
    UnityEvent<int> OnSelectLevel
    + show popup
    + sound
    + etc

---
+++ Hidalgo/Assets/_scripts/SceneManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+using sManager = UnityEngine.SceneManagement.SceneManager;
+
+namespace Managers
+{
+    public class SceneManager : MonoBehaviour
+    {
+        public void LoadSync(string sceneName)
+        {
+            sManager.LoadScene(sceneName);
+        }
+        
+        public void LoadSync(int sceneBuildIndex)
+        {
+            sManager.LoadScene(sceneBuildIndex);
+        }
+
+        public void LoadNextSceneSync()
+        {
+            sManager.LoadScene(sManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ Hidalgo/Assets/_scripts/TriggerLevelSelection.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerLevelSelection : MonoBehaviour
+{
+    //public string sceneLoadName = "";
+    //private UnityEvent OnInteraction;
+    [Header("Numero de nivel asociado - 1:Topdown")]
+    public Enums.Level idLevel;
+    bool canBeActivated = true;
+
+    public LayerMask interactsWith;
+    private KeyCode interactKeyPrimary = KeyCode.E;
+    private KeyCode interactKeySecondary = KeyCode.Space;
+
+    private Managers.HubGameManager _gameManager;
+    private void Awake()
+    {
+        if (!_gameManager)
+            _gameManager = FindObjectOfType<Managers.HubGameManager>();
+
+        //if (OnInteraction == null)
+        //    OnInteraction = new UnityEvent();
+
+        //OnInteraction.AddListener(TestMethod);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!Common.GetLayersFromMask(interactsWith).Contains(collision.gameObject.layer))
+            return;
+
+
+        if ((Input.GetKeyDown(interactKeyPrimary) || Input.GetKeyDown(interactKeySecondary)) && canBeActivated)
+        {
+            canBeActivated = false;
+            TestMethod();
+            //OnInteraction?.Invoke();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ResetTrigger();
+    }
+
+    /// <summary>
+    /// llamar despues de que se cierra el popup del about de nivel
+    /// si es que elige quedarse en la escena / cerrarlo
+    /// </summary>
+    public void ResetTrigger()
+    {
+        this.canBeActivated = true;
+    }
+
+    void TestMethod()
+    {
+        Debug.Log("On interaction callback");
+        _gameManager.LoadScene(this.idLevel);
+        // show popup ( about the scene )
+        // freeze counters to lock the player static
+     
+    }
+}
