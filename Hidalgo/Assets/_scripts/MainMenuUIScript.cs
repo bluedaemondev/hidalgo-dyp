d859de8bcaf5b1a196d8e290394023f82f4434ae
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-05-12 18:33:04 -0300

    menu principal y build index

---
+++ Hidalgo/Assets/_scripts/MainMenuUIScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUIScript : MonoBehaviour
+{
+    public GameObject mainPanel;
+    public GameObject creditsPanel;
+    public GameObject howToPlayPanel;
+
+
+    private void Awake()
+    {
+        ShowMainMenu();
+    }
+    public void ShowMainMenu()
+    {
+        mainPanel.SetActive(true);
+        if (creditsPanel != null)
+            creditsPanel.SetActive(false);
+        howToPlayPanel.SetActive(false);
+    }
+    public void PlayNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ShowCredits()
+    {
+        if (creditsPanel == null)
+            return;
+
+        creditsPanel.SetActive(true);
+        
+        mainPanel.SetActive(false);
+        howToPlayPanel.SetActive(false);
+    }
+    public void ShowHowToPlay()
+    {
+        howToPlayPanel.SetActive(true);
+        
+        mainPanel.SetActive(false);
+        if (creditsPanel != null)
+            creditsPanel.SetActive(false);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
