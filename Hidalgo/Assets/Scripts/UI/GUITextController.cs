64c52e5ae4251c227f6abf702f993c32d24ab935
Laura <mlauraseijas@gmail.com>
2021-05-18 13:15:34 -0300

    Sumé script GUITextController para texto en hud

---
+++ Hidalgo/Assets/Scripts/UI/GUITextController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUITextController : MonoBehaviour
+{
+    public float time = 5; //Seconds to read the text
+
+    void Start()
+    {
+        Destroy(gameObject, time);
+    }
+}
