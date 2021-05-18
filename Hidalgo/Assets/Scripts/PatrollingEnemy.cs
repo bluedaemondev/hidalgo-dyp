a2ace3198ddcd73b8c32535b40b7d175625d22b3
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-05-18 14:55:32 -0300

    fixes enemy patrol

--- Hidalgo/Assets/Scripts/PatrollingEnemy.cs
+++ Hidalgo/Assets/Scripts/PatrollingEnemy.cs
@@ -11,36 +11,53 @@ public class PatrollingEnemy : MonoBehaviour
 
     private float CurrentTravelTime;
     private bool MovingToB;
+
+    private bool canMove = true;
     void Update()
     {
-        if (MovingToB)
+        if (canMove)
         {
-            CurrentTravelTime += Time.deltaTime;
-            if (CurrentTravelTime >= TravelTime)
+            if (MovingToB)
             {
-                MovingToB = false;
-                FlipScale();
+                CurrentTravelTime += Time.deltaTime;
+                if (CurrentTravelTime >= TravelTime)
+                {
+                    MovingToB = false;
+                    FlipScale();
+                    AwaitInPlace(2f);
+                }
             }
-        }
-        else
-        {
-            CurrentTravelTime -= Time.deltaTime;
-            if (CurrentTravelTime <= 0f)
+            else
             {
-                MovingToB = true;
-                FlipScale();
+                CurrentTravelTime -= Time.deltaTime;
+                if (CurrentTravelTime <= 0f)
+                {
+                    MovingToB = true;
+                    FlipScale();
+                    AwaitInPlace(2f);
 
 
+                }
             }
-        }
 
-        transform.position = Vector3.Lerp(PointA.position, PointB.position, CurrentTravelTime / TravelTime);
+            transform.position = Vector3.Lerp(PointA.position, PointB.position, CurrentTravelTime / TravelTime);
+        }
     }
-
+    private void AwaitInPlace(float time)
+    {
+        StartCoroutine(LockMovementFor(time));
+    }
+    private IEnumerator LockMovementFor(float time)
+    {
+        canMove = false;
+        yield return new WaitForSeconds(time);
+        canMove = true;
+    }
     private void FlipScale()
     {
         AuxScale = transform.localScale;
         AuxScale.x = -AuxScale.x;
         transform.localScale = AuxScale;
+        
     }
 }
