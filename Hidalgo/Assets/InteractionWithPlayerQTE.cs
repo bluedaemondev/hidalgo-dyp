af32a7f277f6581f79760506ad9a9c5a2d29fef9
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-05-28 02:45:23 -0300

    elmejorfeedbackdetuvida.net

--- Hidalgo/Assets/InteractionWithPlayerQTE.cs
+++ Hidalgo/Assets/InteractionWithPlayerQTE.cs
@@ -11,6 +11,8 @@ public class InteractionWithPlayerQTE : MonoBehaviour
 
     [SerializeField]
     private GameObject maskInteraction;
+    [SerializeField]
+    private GameObject extraRange;
 
     private Vector2 originalScaleMask;
     private bool canTrigger = true;
@@ -43,6 +45,7 @@ public class InteractionWithPlayerQTE : MonoBehaviour
         tFactor = 0;
         maskInteraction.transform.localScale = this.originalScaleMask;
         canTrigger = true;
+        extraRange.SetActive(true);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -50,6 +53,7 @@ public class InteractionWithPlayerQTE : MonoBehaviour
         if (Common.GetLayersFromMask(interactsWith).Contains(collision.gameObject.layer))
         {
             target = collision.gameObject;
+            extraRange.SetActive(false);
         }
 
     }
@@ -70,20 +74,13 @@ public class InteractionWithPlayerQTE : MonoBehaviour
     }
     void ComputePlayerInRange()
     {
+
         this.timeCurrent += Time.deltaTime;
         if (tFactor < 1)
         {
             tFactor += Time.deltaTime / timeToTriggerQTE;
             maskInteraction.transform.localScale = Vector3.Lerp(originalScaleMask, Vector3.zero, tFactor);
         }
-        //var currentPos = startingPoint.position;
-        //var t = 0f;
-        //while (t < 1)
-        //{
-        //    t += Time.deltaTime / timeTotalInQte;
-        //    handle.position = Vector3.Lerp(currentPos, endingPoint.position, t);
-        //    yield return null;
-        //}
 
         if (timeCurrent >= timeToTriggerQTE || tFactor >= 1)
         {
