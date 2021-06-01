d484ae88456d8005251b9a4543bb6bb9971a6cec
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-06-01 06:13:38 -0300

    Here we go again

---
+++ Hidalgo/Assets/Scripts/FootstepChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepChanger : MonoBehaviour
+{
+    public AudioClip newFootstepSound;
+    public Player player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+   //     player.ChangeFootstepSound(newFootstepSound);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+   //     player.RetrieveFootstepSound();
+    }
+}
