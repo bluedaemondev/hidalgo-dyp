50f2015bbad41a6430c47fe339cc0cc8fa830662
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-06-05 17:24:30 -0300

    FootstepChanger y nuevos Audioclip

--- Hidalgo/Assets/Scripts/Player/Movement.cs
+++ Hidalgo/Assets/Scripts/Player/Movement.cs
@@ -32,6 +32,8 @@ public class Movement
             {
                 SoundManager.instance.PlayEffect(_player.currentFootstepSound);
                 nextSoundTime = Time.time + _player.currentFootstepSound.length;
+
+                Debug.Log(_player.currentFootstepSound);
             }
         }
         else
