ae9cf3364e4c95ccf5358709b0591ba4efca9cf1
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-06-05 04:11:02 -0300

    ArmorParts Events

---
+++ Hidalgo/Assets/Scripts/ArmorParts/GlovesEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlovesEvent : MonoBehaviour
+{
+    public Player player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.ArmorState = 2f;
+        player.SetArmorState();
+    }
+}
