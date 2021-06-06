467678527e70505fb004a9f28a44d1cd27807c15
Laura <mlauraseijas@gmail.com>
2021-06-06 04:05:09 -0300

    no puedo hacer funcionar la cutscene, mañana pruebo de nuevo

---
+++ Hidalgo/Assets/Scripts/Cutscene_Victoria_Trigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene_Victoria_Trigger : MonoBehaviour
+{
+    public GameObject Cutscene_V;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 15) //colisión contra trigger para cutscene en salida
+        {
+            Cutscene_V.SetActive(true); //play cutscene de victoria
+        }
+    }
+}
