e5151824d3ba0975dce1e32c1f344acc97f436e5
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-06-06 18:18:40 -0300

    Fix Gloves

--- Hidalgo/Assets/Scripts/ArmorParts/GlovesEvent.cs
+++ Hidalgo/Assets/Scripts/ArmorParts/GlovesEvent.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class GlovesEvent : MonoBehaviour
 {
     public Player player;
+    public GameObject Wall;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         player.ArmorState = 2f;
         player.SetArmorState();
+
+        if(Wall != null)
+        {
+            ClimateController.instance.LightningEffect(Wall.transform.position);
+            Destroy(Wall);
+        }
     }
 }
