4351b02844718e460e459196d8aac8dc4a98b788
Laura <mlauraseijas@gmail.com>
2021-06-17 02:21:41 -0300

    Armando base bien básica + scrolling camera (top down)

---
+++ Hidalgo/Assets/Scrolling_Camera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling_Camera : MonoBehaviour
+{
+
+    public float speed; 
+
+    void Update()
+    {
+
+        transform.Translate(0, -speed, 0);
+    }
+}
