e03da47fc00ef254370cc678b717eab37c4c7c12
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-06-22 12:14:41 -0300

    SpawnData 1

---
+++ Hidalgo/Assets/Scripts/SpawnData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpawnData : MonoBehaviour
+{
+    public List<GameObject> Enemies = new List<GameObject>();
+    public float timeFromLastSpawn;
+
+    public SpawnData(List<GameObject>newEnemy, float newSpawnTime)
+    {
+        Enemies = newEnemy;
+        timeFromLastSpawn = newSpawnTime;
+    }
+  
+}
