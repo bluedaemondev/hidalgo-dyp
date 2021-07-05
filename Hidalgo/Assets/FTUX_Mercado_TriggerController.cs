089c19dd4306e18621fb26697c3af176ffc52d48
Laura <mlauraseijas@gmail.com>
2021-07-04 22:07:07 -0300

    terminando ftux Mercado

---
+++ Hidalgo/Assets/FTUX_Mercado_TriggerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class FTUX_Mercado_TriggerController : MonoBehaviour
+{
+    public PlayableDirector FTUX_Mercado;
+    public LayerMask layersToInteract;
+
+    void Start()
+    {
+        FTUX_Mercado = GetComponent<PlayableDirector>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 9 && HudPlayerPickupScene.instance.checkedRocinante.activeSelf)
+        {
+            FTUX_Mercado.Play();
+        }
+    }
+}
