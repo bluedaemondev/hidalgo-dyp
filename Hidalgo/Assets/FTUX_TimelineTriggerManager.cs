fc75e2cab39567cc095cbb0579ed25ffed2f2b53
Laura <mlauraseijas@gmail.com>
2021-07-05 20:49:07 -0300

    ajustes ftux

--- Hidalgo/Assets/FTUX_TimelineTriggerManager.cs
+++ Hidalgo/Assets/FTUX_TimelineTriggerManager.cs
@@ -6,8 +6,11 @@ using UnityEngine.Playables;
 public class FTUX_TimelineTriggerManager : MonoBehaviour
 {
     public PlayableDirector FTUX_Timeline_Mercado;
+    public PlayableDirector FTUX_Timeline_MercadoFinal;
     public PlayableDirector FTUX_Timeline_Cementerio;
     public LayerMask layersToInteract;
+
+    bool FTUX_Timeline_Mercado_played = false;
 
     void Start()
     {
@@ -18,11 +21,14 @@ public class FTUX_TimelineTriggerManager : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 9 && HudPlayerPickupScene.instance.checkedRocinante.activeSelf)
+        //Play timeline Mercado inicio y final cuando DQ tiene a Rocinante
+        if (collision.gameObject.layer == 9 && HudPlayerPickupScene.instance.checkedRocinante.activeSelf && FTUX_Timeline_Mercado_played == false)
         {
             FTUX_Timeline_Mercado.Play();
+            FTUX_Timeline_Mercado_played = true;
         }
 
+        //Play timeline Cementerio inicio cuando DQ tiene a Rocinante y pickup guantes
         else if (collision.gameObject.layer == 9 && collision.gameObject.layer == 13 && HudPlayerPickupScene.instance.checkedRocinante.activeSelf)
         {
             FTUX_Timeline_Cementerio.Play();
