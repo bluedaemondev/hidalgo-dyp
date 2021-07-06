a9aabc1ec7de74961a85c3471bf8d8033915c405
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-07-06 03:08:53 -0300

    pickup caja + animacion

--- Hidalgo/Assets/_scripts/PickupBox.cs
+++ Hidalgo/Assets/_scripts/PickupBox.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class PickupBox : MonoBehaviour
 {
     public AudioClip onPickup;
+    public GameObject onPickupTutorial;
 
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Instantiate(onPickupTutorial, transform.position, Quaternion.identity, null);
+
         collision.GetComponent<Player>().InitBoxControls();
         SoundManager.instance.PlayEffect(onPickup);
         Destroy(this.gameObject);
