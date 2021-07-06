e6254b6ec94661b487dbee041760e233ac89e3ef
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-07-05 23:41:34 -0300

    probando cosas 1234567890

--- Hidalgo/Assets/ExtendSpriteWidthBasedOnConnection.cs
+++ Hidalgo/Assets/ExtendSpriteWidthBasedOnConnection.cs
@@ -16,20 +16,26 @@ public class ExtendSpriteWidthBasedOnConnection : MonoBehaviour
     {
 
         //transform.position = newPos;
-        var direction = newPos - startPoint;
+        var direction = startPoint - newPos;
         transform.right = direction * transform.lossyScale.x;
 
         float dist = Vector2.Distance(startPoint, newPos);
-        sprRendToGrow.size = new Vector2(sprRendToGrow.size.x, dist);
+        sprRendToGrow.size = new Vector2(dist, sprRendToGrow.size.y);
+    }
+
+    private void OnEnable()
+    {
+        startPoint = transform.position;
+        UpdateRopeGraphic(targetTransform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(transform.position, targetTransform.position) <= maxDistanceWidth)
-        {
+        //if(Vector2.Distance(transform.position, targetTransform.position) <= maxDistanceWidth)
+        //{
             Debug.Log("in range");
             UpdateRopeGraphic(targetTransform.position);
-        }
+        //}
     }
 }
